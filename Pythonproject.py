578c3ee5ac1563c8b8a75d3c059dc16be470a4de
Butterfly302 <71980911+Butterfly302@users.noreply.github.com>
2020-12-14 11:32:39 -0500

    Update Pythonproject.py

--- Pythonproject.py
+++ Pythonproject.py
@@ -1,21 +1,21 @@
-def main ():
-  print "What is your favorite food?"
-  food_answer = raw_input("Enter your food")
+def main():
+    print "What is your favorite food?"
+    food_answer = raw_input("Enter your food")
 
-  print "Hello" + str(food_answer) + " What is your favorite desert?"
-  desert_answer + raw_input ("Enter your desert")
-  if str(desert_answer) != "Brownies."
+    print "Hello" + str(food_answer) + " What is your favorite desert?"
+    desert_answer + raw_input ("Enter your desert")
+    if str(desert_answer) != "Brownies.":
         print "What you like is not availible"
         return
-  print "What is your favorite dish?"
-    color_answer = raw_input("seafood lasagna")
--
-    if str(color_answer) != "seafood lasagna"
+    print "What is your favorite dish?"
+    food_answer = raw_input("seafood lasagna")
+
+    if str(color_answer) != "seafood lasagna":
         print "What you like is not availible"
         return
 
-        print "When is the last time you cooked?"
-        swallow_answer = raw_input("12pm today")
--
-     if str(swallow_answer) != "ckicken or fish"
+    print "When is the last time you cooked?"
+    swallow_answer = raw_input("12pm today")
+    if str(swallow_answer) != "ckicken" or "fish":
         print "What you like is not availible"
+main()
